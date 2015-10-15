6d30f093ad0b93f7dc5e7da20450c3e951d91520
Liang Wang <leowang87@gmail.com>
2015-10-15 22:45:06 +0200

    Learn Ruby the Hard Way

---
+++ the_hard_way/ex5.rb
@@ -0,0 +1,18 @@
+myName = 'Liang Wang'
+myAge = 28 # in 2015
+myHeight = 183 #cm
+myWeight = 80 #kg
+myEyes = 'Black'
+myTeeth = 'White'
+myHair = 'Black'
+
+puts "let's talk about #{myName}"
+puts "he's #{myHeight} cm tall."
+puts "he's #{myWeight} kg heavy."
+puts "Actually that's not too heavy."
+
+puts "he's got #{myEyes} eyes and #{myHair} hair."
+puts "his teeth are usually #{myTeeth} depending on the coffee."
+
+#this line is tricky, try to get it exactly right
+puts "if I add #{myAge}, #{myHeight}, and #{myWeight} I get #{myAge + myHeight + myWeight}."
